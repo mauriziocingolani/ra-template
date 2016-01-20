a077e2cb123cc70fa64256668789ae4dcc6b06d6
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2016-01-20 15:05:41 +0100

    Totali su statistiche campagna

--- protected/views/campaigns/stats.php
+++ protected/views/campaigns/stats.php
@@ -23,7 +23,7 @@ $this->addBreadcrumb('Campagne', array('/campagne'))->
                 <tr>
                     <th>Campagna</th>
                     <th>Operatori</th>
-                    <th>Nominativi</th>
+                    <th>Tickets</th>
                     <th>De gestire</th>
                     <th>Richiami</th>
                     <th>Chiusi</th>
@@ -34,29 +34,59 @@ $this->addBreadcrumb('Campagne', array('/campagne'))->
             <tbody>
                 <?php
                 $i = 0;
+                $tots = array('totali' => 0, 'dagestire' => 0, 'richiami' => 0, 'chiusi' => 0, 'successi' => 0);
                 foreach ($data as $d) :
                     ?>
                     <tr class="<?php echo ($i++) % 2 == 0 ? 'even' : 'odd'; ?>">
                         <td><?php echo $d['campaign']->Name; ?></td>
                         <td><small><?php echo Html::JoinFromModels($d['campaign']->Users, 'UserName', '<br />', 'User'); ?></small></td>
                         <td style="text-align: center;"><?php echo $d['stats']['totali']; ?></td>
+                        <!-- TOT --><?php $tots['totali']+=$d['stats']['totali']; ?>
                         <td style="text-align: center;">
                             <?php echo $d['stats']['dagestire']; ?>
                             <small>(<?php echo $d['stats']['dagestireperc']; ?>)</small>
                         </td>
+                        <!-- TOT --><?php $tots['dagestire']+=$d['stats']['dagestire']; ?>
                         <td style="text-align: center;"><?php echo $d['stats']['richiami']; ?></td>
+                        <!-- TOT --><?php $tots['richiami']+=$d['stats']['richiami']; ?>
                         <td style="text-align: center;">
                             <?php echo $d['stats']['chiusi']; ?>
                             <small>(<?php echo $d['stats']['chiusiperc']; ?>)</small>
                         </td>
+                        <!-- TOT --><?php $tots['chiusi']+=$d['stats']['chiusi']; ?>
                         <td style="text-align: center;">
                             <?php echo $d['stats']['successi']; ?>
                             <small>(<?php echo $d['stats']['successiperc']; ?>)</small>
                         </td>
+                        <!-- TOT --><?php $tots['successi']+=$d['stats']['successi']; ?>
                         <td style="font-weight: bold;text-align: center;"><?php echo $d['stats']['avanzamentoperc']; ?></td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
+            <tfoot>
+                <tr>
+                    <th colspan="2" />
+                    <th><?php echo $tots['totali']; ?></th>
+                    <th>
+                        <?php echo $tots['dagestire']; ?>
+                        <small>(<?php echo sprintf('%.1f', $tots['dagestire'] / $tots['totali'] * 100); ?>&percnt;)</small>
+                    </th>
+                    <th><?php echo $tots['richiami']; ?></th>
+                    <th>
+                        <?php echo $tots['chiusi']; ?>
+                        <small>(
+                            <?php echo sprintf('%.1f', $tots['totali'] - $tots['dagestire'] > 0 ? $tots['chiusi'] / ($tots['totali'] - $tots['dagestire']) * 100 : 0.0); ?>
+                            &percnt;)</small>
+                    </th>
+                    <th>
+                        <?php echo $tots['successi']; ?>
+                        <small>(
+                            <?php echo sprintf('%.1f', $tots['chiusi'] > 0 ? $tots['successi'] / $tots['chiusi'] * 100 : 0.0); ?>
+                            &percnt;)</small>
+                    </th>
+                    <th></th>
+                </tr>
+            </tfoot>
         </table>
     </div>
 

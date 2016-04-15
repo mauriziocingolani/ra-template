0ff10cb02100c248a2d2ced9a120f051f5fd215c
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2016-04-15 11:07:52 +0200

    Link su selettore _company_pages

--- protected/views/companies/_company_pages.php
+++ protected/views/companies/_company_pages.php
@@ -8,12 +8,12 @@
 
 <p class="<?php if (Yii::app()->controller->id == 'companies' && Yii::app()->controller->action->id == 'company') echo 'active'; ?>">
     <a class="link <?php if (Yii::app()->controller->id == 'companies' && Yii::app()->controller->action->id == 'company') echo 'active'; ?>" 
-       href="/azienda/<?= $model->CompanyID; ?>" 
+       href="<?= Yii::app()->baseUrl; ?>/azienda/<?= $model->CompanyID; ?>" 
        style="margin-top: 20px;">
         <span>anagrafica</span>
     </a>
     <a class="link <?php if (Yii::app()->controller->id == 'companies' && Yii::app()->controller->action->id == 'profile') echo 'active'; ?>" 
-       href="/azienda/<?= $model->CompanyID; ?>/profilazione" 
+       href="<?= Yii::app()->baseUrl; ?>/azienda/<?= $model->CompanyID; ?>/profilazione" 
        style="margin-top: 20px;">
         <span>profilazione</span>
     </a>
